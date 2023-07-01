232d069bd7cd0d9faba5382167325a542d9266ab
rich-taveras <rtaveras@yahoo.com>
2023-07-01 06:19:07 -0400

    save work

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var tagName = prompt("How many characters would you like your password to contain?", "enter tag");
+
+// if (tagName !> 8 && tagName >128 )
+// alert("Password lenght shoulb be between 8 to 128 characters")
+
+function generatePassword() {
+  
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -12,3 +20,20 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// alert ("How many characters would you like your password to contain?", )
+
+// How many characters would you like your password to contain?
+
+// Password length must be more than 7 characters
+// Password length must be less than 129 characters
+
+// Click Ok to confirm including special characters
+// Click Ok to confirm including numeric characters
+// Click Ok to confirm including lowercase characters
+// Click Ok to confirm including uppercase characters
+
+// Must select at least one character type
+
+
+
